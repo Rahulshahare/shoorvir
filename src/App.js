137a91aa167066eee5bc3fbcb92bc1b167b53af5
Rahulshahare <rahulshahare@gmail.com>
2025-05-15 08:36:05 +0530

    working on

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import iconMap from './images/geo-alt-fill.svg';
+import iconDown from './images/caret-down-fill.svg';
 import './App.css';
+import Nav from './component/Nav';
+import Model from './component/Model';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <Model/>
     </div>
   );
 }
