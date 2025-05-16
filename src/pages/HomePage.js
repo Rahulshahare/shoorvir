db3aa6c7ee4b322e6ce7363ab8469a9c851ac674
Rahulshahare <rahulshahare@gmail.com>
2025-05-16 08:33:26 +0530

    Home page added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function HomePage(){
+    return(
+        <div className="container heightAfterNav">
+            <div className="row">
+                <div className="col">
+                    <h1 className="text-center">Welcome to the Home Page</h1>
+                    <p className="text-center">This is a simple home page.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
