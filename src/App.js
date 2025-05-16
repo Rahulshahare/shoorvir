86196bc691b2b2e5603d6133aac4758d90fddafc
Rahulshahare <rahulshahare@gmail.com>
2025-05-16 08:33:07 +0530

    `Added new CSS styles and React Router configuration to App.js and App.css`

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Routes,Route } from 'react-router';
 import Nav from './component/Nav';
 import Model from './component/Model';
 import { UIStore } from './state/AppState';
+import HomePage from './pages/HomePage';
 
 
 function App() {
   const showModel = UIStore.useState(s=>s.showModel);
   //const [showModel, isShowModel] = useState(false)
-  const handleClick =()=>{
-    isShowModel(!showModel);
-  }
+  
   return (
-    <>
+    <div className='page'>
       <Nav/>
       {showModel ? <Model/> : ''}
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+      </Routes>
+    </div>
   );
 }
 
