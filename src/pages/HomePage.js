862d684b4a6583509a045d14fe6f26fd379f76bc
Rahulshahare <rahulshahare@gmail.com>
2025-05-16 10:17:40 +0530

    `Added categoryListBg class and modified button styles in HomePage.js and App.css`

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -18,18 +18,16 @@ function HomePage(){
                     categoryList.map((category, index) => {
                         return (
                             <div className="col col-sm-3 d-grid py-3" key={index}>
-                                <button type="button" className="btn btn-outline border btn-lg" onClick={()=>navigate("/search?category="+category)}>
+                                <button type="button" className="btn btn-outline border btn-lg shadow-sm categoryListBg" onClick={()=>navigate("/search?category="+category)}>
                                     <img className="responsive" src={iconCertified} alt="centified" width={100}/>
                                     <h4 className="text-center">CERTIFIED {category}</h4>
                                 </button> 
                             </div>
                         )
                     })
-                }
-                
-                
-                             
+                }                
             </div>
+            
         </div>
     )
 }
