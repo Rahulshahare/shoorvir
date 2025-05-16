18c71b5ee8e8a4eca9bcf949d46b0368719355b1
Rahulshahare <rahulshahare@gmail.com>
2025-05-16 10:02:40 +0530

    `Added navigation functionality to HomePage and updated Category list in AppState`

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,34 @@
 import React from "react";
+import { useNavigate } from "react-router"
+import iconCertified from './../images/undraw_certificate_71gt.svg'
+import { Category } from "../state/AppState";
 
 function HomePage(){
+    const navigate = useNavigate();
+    const categoryList = Category.useState(s=>s.CategoryList)
     return(
         <div className="container">
-            <div className="row">
-                <div className="col">
+            <div className="row mb-5">
+                <div className="col mt-2">
                     <h1 className="text-center text-secondary">Home services at your doorstep</h1>
                 </div>
+            </div>
+            <div className="row">
+                {
+                    categoryList.map((category, index) => {
+                        return (
+                            <div className="col col-sm-3 d-grid py-3" key={index}>
+                                <button type="button" className="btn btn-outline border btn-lg" onClick={()=>navigate("/search?category="+category)}>
+                                    <img className="responsive" src={iconCertified} alt="centified" width={100}/>
+                                    <h4 className="text-center">CERTIFIED {category}</h4>
+                                </button> 
+                            </div>
+                        )
+                    })
+                }
+                
+                
+                             
             </div>
         </div>
     )
--- src/state/AppState.js
+++ src/state/AppState.js
@@ -4,7 +4,40 @@ export const UIStore = new Store({
     showModel: false,
 });
 
+export const Category = new Store({
+     CategoryList: [
+        "ELECTRICIAN",
+        "PLUMBER",
+        "TEACHER",
+        "RECEPTIONIST",
+        "OFFICE STAFF",
+        "MECHANIC",
+        "ENGINEER",
+        "DRIVER",
+        "DELIVERY BOY",
+        "TAILOR",
+        "GUARD",
+        "BARBER",
+        "CARE TAKER",
+        "LABOURS",
+        "TECHNICIAN",
+        "TIFFIN SERVICES",
+        "ARTIST",
+        "BEAUTICIAN",
+        "TRAINER",
+        "MEDICAL SERVICES",
+        "PHOTOGRAPHER",
+        "CARPENTER",
+        "RAJ MISTRY",
+        "TENT SERVICES",
+        "DECORATION",
+        "CATERING",
+        "DANCE TRAINER"
+      ],
+})
+
 
 registerInDevtools({
     UIStore,
+    Category
 })
