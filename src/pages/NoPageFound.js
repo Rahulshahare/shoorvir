764e283a6f732e46e56147ddd5ff77ea114fced2
Rahulshahare <rahulshahare@gmail.com>
2025-05-17 11:21:48 +0530

    404 page

---
+++ src/pages/NoPageFound.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NoPageFound() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col text-center">
+                    <h1>404</h1>
+                    <h2>Page Not Found</h2>
+                    <p>The page you are looking for does not exist.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NoPageFound;
