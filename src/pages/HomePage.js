0788346fee62975f3cc16171c105027ae8ebabc2
Rahulshahare <rahulshahare@gmail.com>
2025-05-18 16:31:09 +0530

    useNavigate from react-router dom

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,13 @@
 import React, { useRef } from "react";
-import { NavLink } from 'react-router-dom';
+import { useNavigate  } from 'react-router-dom';
 import imageCertified from './../images/undraw_certificate_71gt.svg';
-import iconLeft from './../images/caret-left-fill.svg';
-import iconRight from './../images/caret-right-fill.svg';  
 import { Category } from "../state/AppState";
 
 
 function HomePage(){
     const wrapperRef = useRef(null);
     const categorizedProfessions = Category.useState(s=>s.categorizedProfessions)
+    const navigate = useNavigate();
     
 
     
@@ -26,18 +25,16 @@ function HomePage(){
                     <div className="d-flex overflow-auto py-3 flex-nowrap" id={`wrapper-${index}`}>
                         {professions.map((profession, i) => (
                             <div className="flex-shrink-0 me-3" key={i}>
-                               <NavLink to={`/search?category=${profession}`}>
                                 <button
                                     type="button"
                                     className="btn btn-outline border btn-lg categoryListBg "
-                                    // onClick={() => handleClick(`/search?category=${profession}`)}
+                                    onClick={() => navigate(`/search?category=${profession}`)}
                                 >
                                     <div className="categoryListBg pt-2 mt-2 rounded">
                                         <img className="responsive" src={imageCertified} alt="certified" width={100} />
                                     </div>
                                     <p className="text-center mt-3 font1rem fontWeight600"> {profession}</p>
                                 </button>
-                                </NavLink>
                             </div>
                         ))}
                     </div>
